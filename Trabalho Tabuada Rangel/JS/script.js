01298d545e915aa58f56758c22bccdc52051a601
Emmanuel Nascimento <145400870+Emmanuel-Nascimento@users.noreply.github.com>
2024-04-11 23:38:21 -0300

    Add files via upload

---
+++ Trabalho Tabuada Rangel/JS/script.js
@@ -0,0 +1,254 @@
+//Prefixo ENO vem de Emmanuel Nascimento de Oliveira
+
+const ENO_numero_1 = document.getElementById('um').addEventListener('click', function(){
+    let ENO_numero = 1;
+    let ENO_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_abuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} + ${i} = ${ENO_numero + i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} - ${i} = ${ENO_numero - i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} x ${i} = ${ENO_numero * i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} / ${i} = ${ENO_numero / i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});            
+});
+const ENO_numero_2 = document.getElementById('dois').addEventListener('click', function(){
+    let ENO_numero = 2;
+    let ENO_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} + ${i} = ${ENO_numero + i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} - ${i} = ${ENO_numero - i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} x ${i} = ${ENO_numero * i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} / ${i} = ${ENO_numero / i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});            
+});
+const ENO_numero_3 = document.getElementById('tres').addEventListener('click', function(){
+    let ENO_numero = 3;
+    let ENO_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} + ${i} = ${ENO_numero + i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} - ${i} = ${ENO_numero - i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} x ${i} = ${ENO_numero * i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} / ${i} = ${ENO_numero / i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});            
+});
+const ENO_numero_4 = document.getElementById('quatro').addEventListener('click', function(){
+    let ENO_numero = 4;
+    let ENO_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} + ${i} = ${ENO_numero + i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} - ${i} = ${ENO_numero - i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} x ${i} = ${ENO_numero * i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} / ${i} = ${ENO_numero / i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});            
+});
+const ENO_numero_5 = document.getElementById('cinco').addEventListener('click', function(){
+    let ENO_numero = 5;
+    let ENO_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} + ${i} = ${ENO_numero + i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} - ${i} = ${ENO_numero - i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} x ${i} = ${ENO_numero * i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} / ${i} = ${ENO_numero / i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});            
+});
+const ENO_numero_6 = document.getElementById('seis').addEventListener('click', function(){
+    let ENO_numero = 6;
+    let ENO_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} + ${i} = ${ENO_numero + i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} - ${i} = ${ENO_numero - i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} x ${i} = ${ENO_numero * i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} / ${i} = ${ENO_numero / i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});            
+});
+const ENO_numero_7 = document.getElementById('sete').addEventListener('click', function(){
+    let ENO_numero = 7;
+    let ENO_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} + ${i} = ${ENO_numero + i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} - ${i} = ${ENO_numero - i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} x ${i} = ${ENO_numero * i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} / ${i} = ${ENO_numero / i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});            
+});
+const ENO_numero_8 = document.getElementById('oito').addEventListener('click', function(){
+    let ENO_numero = 8;
+    let ENO_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} + ${i} = ${ENO_numero + i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} - ${i} = ${ENO_numero - i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} x ${i} = ${ENO_numero * i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} / ${i} = ${ENO_numero / i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});            
+});
+const ENO_numero_9 = document.getElementById('nove').addEventListener('click', function(){
+    let ENO_numero = 9;
+    let ENO_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} + ${i} = ${ENO_numero + i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} - ${i} = ${ENO_numero - i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} x ${i} = ${ENO_numero * i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(ENO_tabuada != ''){ENO_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                ENO_tabuada += `${ENO_numero} / ${i} = ${ENO_numero / i}<br>`;
+                resultado.innerHTML = ENO_tabuada;
+            }});            
+});
